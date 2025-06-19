2f31b85cbe6548ffc032aa2857e00094b0f85a48
JangCoding <jjangga813@naver.com>
2025-06-19 14:28:29 +0900

    [init Commit] 프로젝트 생성 커밋 -SpringBoot -JDK21 / JAVA21 -MyBatis -MySQL 8.0.33

---
+++ src/main/java/org/genasona/grizago/GrizaGoApplication.java
@@ -0,0 +1,13 @@
+package org.genasona.grizago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrizaGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrizaGoApplication.class, args);
+    }
+
+}
